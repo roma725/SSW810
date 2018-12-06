5818cdf62805ee5bb13451bb6a726ca42968e1e2
Roma S <21241863+roma725@users.noreply.github.com>
2018-12-06 04:09:35 -0500

    Add files via upload

---
+++ HW10rshah.py
@@ -0,0 +1,6 @@
+'''ROma Shah
+HW 10
+Add the new feature to your solution to show the remaining required
+ courses for each student.  This requires information about required 
+ courses and electives for each major.
+'''
